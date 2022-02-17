57b57096f732ed887b391a1a0fa2a92c569c53d9
Jaime Muela <jaimuepe@gmail.com>
2022-02-17 19:16:48 +0100

    parser

--- src/com/hashbullas/hashcode/program/AppMain.java
+++ src/com/hashbullas/hashcode/program/AppMain.java
@@ -4,19 +4,36 @@ import java.awt.*;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import com.hashbullas.hashcode.files.FileUtils;
 import com.hashbullas.hashcode.parser.Parser;
 
+class ClientPreferences {
+
+    public Set<String> likedIngredients;
+    public Set<String> dislikedIngredients;
+
+    public ClientPreferences() {
+        likedIngredients = new HashSet<>();
+        dislikedIngredients = new HashSet<>();
+    }
+}
+
 public class AppMain {
 
     public static String[] problemsToParse = new String[]{
-            "a_example.in",
-            "b_small.in",
-            "c_medium.in",
-            "d_quite_big.in",
-            "e_also_big.in",
+            "a_an_example.in.txt",
+            "b_basic.in.txt",
+            "c_coarse.in.txt",
+            "d_difficult.in.txt",
+            "e_elaborate.in.txt",
     };
+
+    public static List<ClientPreferences> clientPreferences;
 
     public static void main(String[] args) throws Exception {
 
@@ -34,6 +51,40 @@ public class AppMain {
 
         Parser p = new Parser(problemInput);
 
+        var nClients = p.valueAt(0, 0, Integer.class);
+
+        clientPreferences = new ArrayList<>(nClients);
+
+        for (int i = 0; i < nClients; i++) {
+
+            var firstRow = 2 * i + 1;
+            var secondRow = 2 * i + 2;
+
+            var client = new ClientPreferences();
+
+            var nLikedIngredients = p.valueAt(firstRow, 0, Integer.class);
+
+            List<String> likedIngredients = new ArrayList<>(nLikedIngredients);
+
+            for (int j = 0; j < nLikedIngredients; j++) {
+                likedIngredients.add(p.valueAt(firstRow, j + 1, String.class));
+            }
+
+            client.likedIngredients = new HashSet<>(likedIngredients);
+
+            var nDislikedIngredients = p.valueAt(secondRow, 0, Integer.class);
+
+            List<String> dislikedIngredients = new ArrayList<>(nDislikedIngredients);
+
+            for (int j = 0; j < nDislikedIngredients; j++) {
+                dislikedIngredients.add(p.valueAt(secondRow, j + 1, String.class));
+            }
+
+            client.dislikedIngredients = new HashSet<>(dislikedIngredients);
+
+            clientPreferences.add(client);
+        }
+
         // TODO parse logic
         // ...
 
@@ -50,7 +101,7 @@ public class AppMain {
     private static void writeSolutionToFile(String inputFileName, String solution) throws IOException {
 
         Path outPath = Paths.get(System.getenv("userprofile")).resolve("hashcode-results")
-                .resolve(inputFileName.substring(0, inputFileName.length() - 3) + ".out");
+                .resolve(inputFileName.substring(0, inputFileName.length() - 7) + ".out");
 
         FileUtils.writeFile(outPath, solution);
 
